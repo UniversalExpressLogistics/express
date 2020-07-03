dfeda950285eb77d8b58e87116d47c90e3626795
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2020-07-04 01:29:29 +0530

    Contact us form updated

--- email/contact_us.php
+++ email/contact_us.php
@@ -1,12 +1,13 @@
 <?
-$name = 'Name : ' . $_POST['fname']; . "\r\n" . $_POST['lname'];
+$name = 'Name : ' . $_POST['name'];
+$contact = 'Contact : ' . $_POST['contact'];
 $email = $_POST['email'];
 $subject = 'Subject : ' . $_POST['subject'];
 $body = 'Body : ' . $_POST['message'];
 
 $to      = 'universal.amanaggarwal@gmail.com';
-$message = $name . "\r\n" . $subject . "\r\n"  . $body;
-$subject = 'Inquiry from website';
+$message = $name . "\r\n" . $contact ."\r\n" . $subject . "\r\n"  . $body;
+$subject = 'Inquiry from Contact us page of Website';
 $headers = 'From: ' . $email . "\r\n" .
     'Reply-To: universal.amanaggarwal@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
