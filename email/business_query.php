aa715579ac9c99be5b7848ce23e78ab6f105e308
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2020-07-04 01:25:03 +0530

    Contact information updated

--- email/business_query.php
+++ email/business_query.php
@@ -2,9 +2,10 @@
 $name = 'Name : ' . $_POST['fq_name'];
 $email = $_POST['fq_email'];
 $body = 'Body : ' . $_POST['fq_text'];
+$contact = 'Contact : ' . $_POST['fq_contact'];
 
 $to      = 'universal.amanaggarwal@gmail.com';
-$message = $name . "\r\n"  . $body;
+$message = $name  . "\r\n" . $contact . "\r\n"  . $body;
 $subject = 'Business query from website';
 $headers = 'From: ' . $email . "\r\n" .
     'Reply-To: universal.amanaggarwal@gmail.com' . "\r\n" .
