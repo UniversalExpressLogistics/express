d9d81d2052495995408406c99488c4e9a21d9c7b
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2020-07-06 20:52:12 +0530

    Update business_query.php

--- email/business_query.php
+++ email/business_query.php
@@ -12,10 +12,9 @@ $headers = 'From: ' . $email . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 if (mail($to, $subject, $message, $headers)) {
-    // header('location: ../index.html');
     echo"<script language='javascript'>alert('Mail Sent Successfully')</script>";
 } else {
-    // header('location: ../404.html');
     echo"<script language='javascript'>alert('Mail Failed, please try again')</script>";
 }
+header('location: ../index.html');
 ?>
